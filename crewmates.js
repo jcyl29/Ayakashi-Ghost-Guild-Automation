193652f0252147c8f04a30d64eba8249b655717d
Justin Lui <jlui@surveymonkey.com>
2015-01-26 14:53:55 -0800

    added seeking new cremates code

--- crewmates.js
+++ crewmates.js
@@ -4,3 +4,11 @@ $('.button.touch-button.mini').not(".locked").each(function (i, el) {
     }, i * 2000);
 
 });
+
+$('button').filter(function (a, b) {
+    return b.textContent.trim() == "Crewmate Request";
+}).each(function (i, el) {
+    setTimeout(function () {
+        el.click();
+    }, i * 1000);
+});
