b0d0b9f500c35db2b670ef7ad677e2fabfcf6377
Justin Lui <justincylui@gmail.com>
2015-02-08 11:58:35 -0800

    first poke button does not have mini class

--- crewmates.js
+++ crewmates.js
@@ -1,4 +1,4 @@
-$('.button.touch-button.mini').not(".locked").each(function (i, el) {
+$('.button.touch-button').not(".locked").each(function (i, el) {
     setTimeout(function () {
         el.click();
     }, i * 2000);
