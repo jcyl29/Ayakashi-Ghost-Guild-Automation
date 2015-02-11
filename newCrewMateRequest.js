5a885c5e605777e4fc3970b4cf3ea6d65ec23e68
Justin Lui <jlui@surveymonkey.com>
2015-02-11 12:35:37 -0800

    pycharm reformatting

--- newCrewMateRequest.js
+++ newCrewMateRequest.js
@@ -1,18 +1,18 @@
 function crewmateRequest() {
 
-	var $buttons = $('button').filter(function (a, b) {
-	    return b.textContent.trim() == "Crewmate Request"
-	}),
-	crewmatesPerPage = $buttons.length;
+    var $buttons = $('button').filter(function (a, b) {
+            return b.textContent.trim() == "Crewmate Request"
+        }),
+        crewmatesPerPage = $buttons.length;
 
-	$buttons.each(function (i, el) {
-	    setTimeout(function () {
-	        el.click();
-	        if (i + 1 === crewmatesPerPage) {
-	        	$('button#update-user-list').click();
-	        	setTimeout(crewmateRequest, 3000);
-	        }
-	    }, i * 1000);
-	});	
+    $buttons.each(function (i, el) {
+        setTimeout(function () {
+            el.click();
+            if (i + 1 === crewmatesPerPage) {
+                $('button#update-user-list').click();
+                setTimeout(crewmateRequest, 3000);
+            }
+        }, i * 1000);
+    });
 }
 crewmateRequest();
